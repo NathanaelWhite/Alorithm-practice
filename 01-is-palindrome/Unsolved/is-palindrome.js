2a6a140704a13729085ef6170ddc6b69e1916fce
Nathanael White <narhanaelw99@gmail.com>
2022-02-01 20:48:23 -0600

    palindrome algorithm again

--- 01-is-palindrome/Unsolved/is-palindrome.js
+++ 01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,11 +1,13 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {
-    var newString = str.split("").reverse().join("");
+var isPalindrome = function (str) {
+  var newStr = str;
 
-    if (newString === str) {
-        return true
-    } else {
-        return false
-    }
+  var reverseStr = str.split("").reverse().join("");
+
+  if (newStr === reverseStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
--- 02-is-unique/Unsolved/is-unique.js
+++ 02-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,8 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        
+    }
+};
