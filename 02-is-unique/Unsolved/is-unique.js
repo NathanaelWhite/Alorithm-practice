06680b12a31ebe83b2663d8fce87d6f1334d4b72
Nathanael White <narhanaelw99@gmail.com>
2022-02-03 22:05:09 -0600

    in-unique algorithm

--- 02-is-unique/Unsolved/is-unique.js
+++ 02-is-unique/Unsolved/is-unique.js
@@ -1,8 +1,13 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        
+var isUnique = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let x = 0; i < arr.length; i++) {
+      if (i !== x && arr[i] === arr[x]) {
+        return false;
+      }
     }
+  }
+  return true;
 };
